3f17583851594c97afd2d5a4333520d583432bc9
oreoA09 <oreo.akanni@gmail.com>
2022-06-25 11:21:42 +0000

    create all views & templates

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from .models import Post
 
 # Create your views here.
 def post_list_view(request):
+
+    model = Post
+
     return render(request, 'blog/post_list_view.html', {})
+
+def post_create_view(request):
+
+    model = Post
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')
+
+    return render(request, 'post_form_view.html', {})
+
+def post_detail_view(request):
+
+    model = Post
+    return render(request, 'post_detail_view.html', {})
+
+def post_update_view(request):
+
+    model = Post
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')
+
+    return render(request, 'post_form_view.html', {})
+
+def post_delete_view(request):
+
+    model = Post
+    fields = '__all__'
+    success_url  = reverse_lazy('blog:all')
+
+    return render(request, 'post_delete_view.html', {})
