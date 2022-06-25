2a0ce80758bd61d2c507c2af9b82b0bdf27382cb
oreoA09 <oreo.akanni@gmail.com>
2022-06-25 11:32:07 +0000

    edit urls.py files

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
-    path('', views.post_list_view, name='post_list_view'),
+    path('', views.post_list_view.as_view(), name='all'),
+    path("create/", views.post_create_view.as_view(), name="post_create"),
+    path("delete/<slug:slug>", views.post_delete_view.as_view(), name="post_delete"),
+    path("update/<slug:slug>", views.post_update_view.as_view(), name="post_update"),
+    path("read/<slug:slug>", views.post_detail_view.as_view(), name="post_detail"),
 ]
